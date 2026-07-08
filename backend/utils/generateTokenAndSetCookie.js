84359b03c09dab1fae12a33a285847bd72df9900
Faithfulness Oluwafemi <faithfulness523@gmail.com>
2026-07-08 15:34:19 +0100

    changes

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     res.cookie("auth-token", token, {
         httpOnly: true, // prevents XSS  
         secure: true && process.env.NODE_ENV === 'production', // activates only when it's in production
-        sameSite: 'strict', // prevents CSRF
+        sameSite: 'none', // prevents CSRF
         maxAge: 7 * 24 * 60 * 60 * 1000, // expiration date
         path: "/",
     })
