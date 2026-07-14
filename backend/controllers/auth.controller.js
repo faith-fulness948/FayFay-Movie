400969cba8264f4b5275fe771bd0a324a8a2385b
Faithfulness Oluwafemi <faithfulness523@gmail.com>
2026-07-14 12:11:54 +0100

    nmk

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -228,6 +228,24 @@ export const login = async (req, res) => {
     }
 }
 
+export const getCurrentUser = async (req, res) => {
+    try {
+
+        res.status(200).json({
+            success: true,
+            user: req.user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
 export const forgotPassword = async (req, res) => {
     try {
         const { email } = req.body;
