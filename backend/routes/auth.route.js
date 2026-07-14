a187696dafe382ed3912bb5372c8540dd1d32871
Faithfulness Oluwafemi <faithfulness523@gmail.com>
2026-07-14 12:37:52 +0100

    mm

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getCurrentUser } from '../controllers/auth.controller.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 import { forgotPassword, login, logout, resetPassword, signup, verifyEmail } from '../controllers/auth.controller.js';
 
 const router = express.Router();
@@ -13,5 +15,7 @@ router.route("/logout").post(logout);
 router.route("/forgot-password").post(forgotPassword);
 
 router.route("/reset-password/:token").post(resetPassword);
+
+router.route("/me").get(authMiddleware, getCurrentUser);
 
 export default router;
