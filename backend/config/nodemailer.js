99f8eacf097e2038e8bac74296cf807aea7caa29
Faithfulness Oluwafemi <faithfulness523@gmail.com>
2026-07-21 12:23:19 +0100

    mistake

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,37 +1,35 @@
 import nodemailer from "nodemailer";
-// import "dotenv/config"
-
-// Create a transporter
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD, // The 16-character App Password
-  },
-});
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
 
-// const mailOptions = {
-//     from: `MongoDB_Class <${process.env.NODEMAILER_USER}`,
-//     to: "ayodejiaronimo@gmail.com",
-//     subject: "Test Email from Nodemailer",
-//     text: "This is just a test!",
-//     html: `<h1>This is a test from stubborn Fayfay</h1>`,
-// }
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 const sendEmail = async (mailOptions) => {
     try {
+        console.log("Sending email to:", mailOptions.to);
+
         const finalOptions = {
-            from: `MongoDB_Class <${process.env.EMAIL_USER}>`,
-            ...mailOptions,
-        }
+            from: `FayFay Movie <${process.env.EMAIL_USER}>`,
+            ...mailOptions
+        };
+
+        console.log("About to send email...");
 
         const info = await transporter.sendMail(finalOptions);
-        console.log("Email Successful:", info.messageId);
+
+        console.log("Email sent successfully:", info.messageId);
+
     } catch (error) {
-        console.error("Email Error:", error);       
+        console.error("Email Error:", error);
+        throw error;
     }
-}
+};
 
 export default sendEmail;
-
