36372af5fdf3ffedea4b26597d06bb6c918fc8af
Faithfulness Oluwafemi <faithfulness523@gmail.com>
2026-07-28 13:10:26 +0100

    changes

--- backend/models/movie.model.js
+++ backend/models/movie.model.js
@@ -40,6 +40,11 @@ const movieSchema = new mongoose.Schema(
             type: String,
             maxLength: 200,
         },
+            // Official YouTube trailer
+        trailerLink: {
+            type: String,
+            required: true
+        },
 
     },
     {
